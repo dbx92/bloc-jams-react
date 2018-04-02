fc4039f06e4a7519d18d4aae6ffdc5a261defd71
dbx92 <davidpiobergamo@comcast.net>
2018-04-01 20:43:05 -0400

    range-inputs

--- src/components/PlayerBar.js
+++ src/components/PlayerBar.js
@@ -6,23 +6,39 @@ class PlayerBar extends Component {
       <section className = 'player-bar'>
         <section id='buttons'>
           <button id='previous' onClick={this.props.handlePrevClick}>
-            <span className='ion-skip-backward'></span>
+            <span className='ion-skip-backward'> </span>
           </button>
           <button id='play-pause' onClick={this.props.handleSongClick}>
             <span className={this.props.isPlaying ? 'ion-pause' : 'ion-play'}></span>
           </button>
           <button id='next' onClick={this.props.handleNextClick}>
-            <span className='ion-skip-forward'></span>
+            <span className='ion-skip-forward'> </span>
           </button>
         </section>
         <section id="time-control">
-          <div className="current-time">–:––</div>
-          <input type="range" className="seek-bar" value="0" />
-          <div className="total-time">–:––</div>
+          <div className="current-time">{this.props.formatTime(this.props.currentTime)}</div>
+          <input
+            type="range"
+            className="seek-bar"
+            value={(this.props.currentTime / this.props.duration) || 0}
+            max='1'
+            min='0'
+            step='0.1'
+            onChange={this.props.handleTimeChange}
+             />
+          <div className="total-time">{this.props.formatTime(this.props.duration || 0)}</div>
         </section>
         <section id="volume-control">
           <div className="icon ion-volume-low"></div>
-          <input type="range" className="seek-bar" value="80" />
+          <input
+              type="range"
+              className="seek-bar"
+              value={this.props.currentVolume}
+              max="10"
+              min="1"
+              step="1"
+              onChange={this.props.handleVolumeChange}
+               />
           <div className="icon ion-volume-high"></div>
         </section>
       </section>
